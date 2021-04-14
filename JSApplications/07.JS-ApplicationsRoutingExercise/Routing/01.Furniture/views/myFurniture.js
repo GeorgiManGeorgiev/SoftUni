ef534324c118da07dd7c1b5209c8a7ab6de7fc0c
GeorgiManGeorgiev <georgi_gmg@abv.bg>
2021-04-14 13:38:56 +0300

    Added Routing exercise

---
+++ JSApplications/07.JS-ApplicationsRoutingExercise/Routing/01.Furniture/views/myFurniture.js
@@ -0,0 +1,23 @@
+//import {html} from '../../node_modules/lit-html/lit-html.js';
+import {html} from 'https://unpkg.com/lit-html?module';
+import {getMyFurniture} from '../api/data.js';
+import {itemTemplate} from "./common/item.js";
+
+
+const myTemplate = (data)=> html`
+    <div class="row space-top">
+        <div class="col-md-12">
+            <h1>My Furniture</h1>
+            <p>This is a list of your publications.</p>
+        </div>
+    </div>
+    <div class="row space-top">
+       ${data.map(itemTemplate)} 
+    </div>
+
+`;
+
+export async function myFurniturePage(ctx){
+    const data = await getMyFurniture()
+    ctx.render(myTemplate(data));
+}
