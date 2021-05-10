215ec6d359fdc9d4f2206b199e9818eee98c7ca5
GeorgiManGeorgiev <georgi_gmg@abv.bg>
2021-05-11 00:02:57 +0300

    Added authentication

--- JSWeb/JSBackEnd/Cubicle/controllers/authController.js
+++ JSWeb/JSBackEnd/Cubicle/controllers/authController.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
-
+const authService = require('../services/authService');
 router.get('/login',(req,res)=>{
     res.render('login');
 });
 
 router.get('/register',(req,res)=>{
     res.render('register');
+});
+
+router.post('/register',async (req,res)=>{
+//     try {
+// const
+//     }catch (error){
+
+
+    }
+
+
+    res.redirect('/auth/register');
 });
 
 
