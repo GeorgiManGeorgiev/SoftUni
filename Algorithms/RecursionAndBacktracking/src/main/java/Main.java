cab88b4780b434ce267d3bae7edd0d38689b5deb
GeorgiManGeorgiev <georgi_gmg@abv.bg>
2021-05-21 23:40:26 +0300

    Added RecursionAndBacktracking

--- Algorithms/RecursionAndBacktracking/src/main/java/Main.java
+++ Algorithms/RecursionAndBacktracking/src/main/java/Main.java
@@ -5,9 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-//calculateFactorial
+//01 calculateFactorial
+
 //        long result = calculateFactorial(5);
 //        System.out.println(result);
+
+
+// 02
+//        recursiveDrawing(5);
+
+
+// 03 generateBitVector
+//        Integer[] memory = new Integer[6];
+//        generateBitVector(memory, 0);
 
 
     }
@@ -20,6 +30,44 @@ public class Main {
         return n * calculateFactorial(n - 1);
     }
 
+    private static void recursiveDrawing(int n) {
 
+        if (n == 0) {
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+
+        recursiveDrawing(n - 1);
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("#");
+        }
+        System.out.println();
+
+    }
+
+    private static void generateBitVector(Integer[] memory, int index) {
+
+        if (index >= memory.length) {
+            for (Integer integer : memory) {
+                System.out.print(integer);
+            }
+
+            System.out.println();
+            return;
+        }
+
+
+        for (int i = 0; i <= 1; i++) {
+            memory[index] = i;
+            generateBitVector(memory, index + 1);
+        }
+    }
 
 }
