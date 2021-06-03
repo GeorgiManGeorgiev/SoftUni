7f2c4500ca6494ca08a90836e4aec1f6e23a6504
GeorgiManGeorgiev <georgi_gmg@abv.bg>
2021-06-03 11:33:58 +0300

    Added RecursionAndBacktracking

--- Algorithms/RecursionAndBacktracking/src/main/java/Main.java
+++ Algorithms/RecursionAndBacktracking/src/main/java/Main.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
+import java.util.List;
 public class Main {
 
+    static List<Character> path = new ArrayList<>();
+
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
 //01 calculateFactorial
@@ -28,27 +34,54 @@ public class Main {
             labyrinth[row] = scanner.nextLine().toCharArray();
         }
 
-        findPath(labyrinth, 0, 0);
+        findPath(labyrinth, 0, 0, ' ');
 
     }
 
 
-    private static void findPath(char[][] labyrinth, int row, int col) {
+    private static void findPath(char[][] labyrinth, int row, int col, char direction) {
 
-        if (!isInBounds(labyrinth, row, col)) {
+        if (!isInBounds(labyrinth, row, col)
+                || labyrinth[row][col] == 'V'
+                || labyrinth[row][col] == '*') {
             return;
 
         }
+
+        path.add(direction);
+
+        if (labyrinth[row][col] == 'e') {
+            printPath();
+            path.remove(path.size()-1);
+            return;
+
+        }
+
+
         labyrinth[row][col] = 'V';
 
-        findPath(labyrinth,row,col);
+        findPath(labyrinth, row - 1, col, 'U');
+        findPath(labyrinth, row + 1, col, 'D');
+        findPath(labyrinth, row, col - 1, 'L');
+        findPath(labyrinth, row, col + 1, 'R');
+
+        labyrinth[row][col]= '-';
+
+        path.remove(path.size()-1);
+
+    }
+
+    private static void printPath() {
+
+        for (int i = 1; i < path.size(); i++)  {
+            System.out.print(path.get(i));
+        }
+        System.out.println();
     }
 
     private static boolean isInBounds(char[][] labyrinth, int row, int col) {
         return row < labyrinth.length && row >= 0 && col < labyrinth[row].length && col >= 0;
     }
-
-
 
 
     private static long calculateFactorial(int n) {
