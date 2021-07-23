8217d7e2476878733d27153886602e914d87abc2
GeorgiManGeorgiev <georgi_gmg@abv.bg>
2021-07-23 17:02:22 +0300

    Added offer and engine and transmission

---
+++ SpringFundamentals/mobilele/src/main/java/entities/OfferEntity.java
@@ -0,0 +1,118 @@
+package entities;
+
+import entities.enums.EngineEnum;
+import entities.enums.TransmissionEnum;
+import org.springframework.ui.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "offers")
+public class OfferEntity extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    private EngineEnum engine;
+    private String description;
+    private String imageUrl;
+    private int mileage;
+    private int price;
+    @Enumerated(EnumType.STRING)
+    private TransmissionEnum transmission;
+
+    private int year;
+
+    @ManyToOne
+    private ModelEntity model;
+
+
+    //TODO seller
+
+    public EngineEnum getEngine() {
+        return engine;
+    }
+
+    public OfferEntity setEngine(EngineEnum engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public OfferEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public OfferEntity setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public OfferEntity setMileage(int mileage) {
+        this.mileage = mileage;
+        return this;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public OfferEntity setPrice(int price) {
+        this.price = price;
+        return this;
+    }
+
+    public TransmissionEnum getTransmission() {
+        return transmission;
+    }
+
+    public OfferEntity setTransmission(TransmissionEnum transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public OfferEntity setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public ModelEntity getModel() {
+        return model;
+    }
+
+    public OfferEntity setModel(ModelEntity model) {
+        this.model = model;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferEntity{" +
+                "id=" + id +
+                ", created=" + created +
+                ", modified=" + modified +
+                ", engine=" + engine +
+                ", description='" + description + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", mileage=" + mileage +
+                ", price=" + price +
+                ", transmission=" + transmission +
+                ", year=" + year +
+                ", model=" + model +
+                '}';
+    }
+}
---
+++ SpringFundamentals/mobilele/src/main/java/entities/enums/EngineEnum.java
@@ -0,0 +1,8 @@
+package entities.enums;
+
+public enum EngineEnum {
+    GASOLINE,
+    DIESEL,
+    ELECTRIC,
+    HYBRID
+}
